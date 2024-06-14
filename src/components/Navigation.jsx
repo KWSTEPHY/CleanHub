aa519fac6284b41f3a552c738b0fd9eac1cee639
victor@victor <victor.dmaina@gmail.com>
2024-06-14 16:17:20 +0300

    reconfigure chakra to tailwind

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,56 +1,52 @@
 import React, { useState } from "react";
+import { FaHome, FaUser, FaComments, FaCamera, FaCog } from "react-icons/fa";
 
 const Navigation = () => {
   const Menus = [
-    { name: "Home", icon: "home-outline", dis: "translate-x-0" },
-    { name: "Profile", icon: "person-outline", dis: "translate-x-16" },
-    { name: "Message", icon: "chatbubble-outline", dis: "translate-x-32" },
-    { name: "Photos", icon: "camera-outline", dis: "translate-x-48" },
-    { name: "Settings", icon: "settings-outline", dis: "translate-x-64" },
+    { name: "Home", icon: FaHome, dis: "translate-x-0" },
+    { name: "Profile", icon: FaUser, dis: "translate-x-16" },
+    { name: "Message", icon: FaComments, dis: "translate-x-32" },
+    { name: "Photos", icon: FaCamera, dis: "translate-x-48" },
+    { name: "Settings", icon: FaCog, dis: "translate-x-64" },
   ];
   const [active, setActive] = useState(0);
+
   return (
-    <div className="bg-white max-h-[4.4rem] px-6 rounded-t-xl">
-      <ul className="flex relative">
-        <span
-          className={`bg-rose-600 duration-500 ${Menus[active].dis} border-4 border-gray-900 h-16 w-16 absolute
-         -top-5 rounded-full`}
-        >
-          <span
-            className="w-3.5 h-3.5 bg-transparent absolute top-4 -left-[18px] 
-          rounded-tr-[11px] shadow-myShadow1"
-          ></span>
-          <span
-            className="w-3.5 h-3.5 bg-transparent absolute top-4 -right-[18px] 
-          rounded-tl-[11px] shadow-myShadow2"
-          ></span>
-        </span>
+    <div className="bg-white w-full fixed top-0 z-10 shadow-md h-24">
+      <div className="flex justify-center items-center relative h-30">
         {Menus.map((menu, i) => (
-          <li key={i} className="w-16">
-            <a
-              className="flex flex-col text-center pt-6"
+          <div key={i} className="w-16 text-center pt-6">
+            <button
+              className={`text-xl cursor-pointer duration-500 ${
+                i === active ? "-mt-6 text-white" : ""
+              }`}
               onClick={() => setActive(i)}
             >
-              <span
-                className={`text-xl cursor-pointer duration-500 ${
-                  i === active && "-mt-6 text-white"
-                }`}
-              >
-                <ion-icon name={menu.icon}></ion-icon>
-              </span>
-              <span
-                className={` ${
-                  active === i
-                    ? "translate-y-4 duration-700 opacity-100"
-                    : "opacity-0 translate-y-10"
-                } `}
-              >
-                {menu.name}
-              </span>
-            </a>
-          </li>
+              <span className="inline-block">{menu.icon()}</span>
+            </button>
+            <span
+              className={`block ${
+                active === i
+                  ? "translate-y-4 duration-700 opacity-100"
+                  : "opacity-0 translate-y-10"
+              }`}
+            >
+              {menu.name}
+            </span>
+          </div>
         ))}
-      </ul>
+        <div
+          className={`bg-rose-600 duration-500 absolute ${Menus[active].dis} border-4 border-gray-900 h-16 w-16
+          -top-5 rounded-full`}
+        >
+          <span
+            className="w-3.5 h-3.5 bg-transparent absolute top-4 -left-7 rounded-tr-lg shadow-myShadow1"
+          ></span>
+          <span
+            className="w-3.5 h-3.5 bg-transparent absolute top-4 -right-7 rounded-tl-lg shadow-myShadow2"
+          ></span>
+        </div>
+      </div>
     </div>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// vite.config.js
+// import { defineConfig } from 'vite';
+// import reactRefresh from '@vitejs/plugin-react-refresh';
+// import tailwindcss from 'tailwindcss';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [reactRefresh(), tailwindcss('./tailwind.config.js')],
+// });
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8001,
+  },
 })
