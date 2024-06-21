5b0c84200e7f3309eb1d607c9d6e0786c790e4d5
victor@victor <victor.dmaina@gmail.com>
2024-06-22 01:51:57 +0300

    add section to Home

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,16 +2,15 @@ import React from "react";
 import { MdAccountCircle } from "react-icons/md";
 import { ImLocation2 } from "react-icons/im";
 import { GiVacuumCleaner } from "react-icons/gi";
-import { MdOutlineSentimentSatisfiedAlt, MdOutlineLocalLaundryService } from "react-icons/md";
+import {
+  MdOutlineSentimentSatisfiedAlt,
+  MdOutlineLocalLaundryService,
+} from "react-icons/md";
 import { SiCcleaner } from "react-icons/si";
-
-
-
-
 
 const cardData = [
   {
-    icon:<GiVacuumCleaner className="text-[#002147] ml-8 mt-4 w-20 h-20" />,
+    icon: <GiVacuumCleaner className="text-[#002147] ml-8 mt-4 w-20 h-20" />,
     title: "Property ",
     price: "$199",
     description: "One Time Installation",
@@ -23,8 +22,10 @@ const cardData = [
       "Floor cleaning",
     ],
   },
-  { 
-    icon:<MdOutlineLocalLaundryService className="text-[#002147] ml-6 mt-6 w-20 h-20" />,
+  {
+    icon: (
+      <MdOutlineLocalLaundryService className="text-[#002147] ml-6 mt-6 w-20 h-20" />
+    ),
     title: "Property ",
     price: "$299",
     description: "One Time Installation",
@@ -37,7 +38,7 @@ const cardData = [
     ],
   },
   {
-    icon:<SiCcleaner className="text-[#002147] ml-4 mt-6 w-20 h-20" />,
+    icon: <SiCcleaner className="text-[#002147] ml-4 mt-6 w-20 h-20" />,
     title: "Property ",
     price: "$399",
     description: "One Time Installation",
@@ -50,7 +51,6 @@ const cardData = [
     ],
   },
 ];
-
 
 const Home: React.FC = () => {
   const cardStyle =
@@ -163,7 +163,7 @@ const Home: React.FC = () => {
           </div>
         </div>
 
-        <div className="p-8 pt-24 flex space-x-8 text-[#002147]">
+        <div className="p-16 pt-24 flex space-x-8 text-[#002147]">
           <div className="img flex-shrink-0 w-2/5 pt-8 ">
             <img
               src="/src/assets/CCCLean.jpg"
@@ -261,83 +261,143 @@ const Home: React.FC = () => {
             <p className="mt-8 font-semibold">Rosalina D. William Founder</p>
           </div>
         </div>
-      </section>
-      {/* <section>
-        <div className="bg-[#002147] p-8 flex justify-around items-start  ">
-        {cardData.map((card, index) => (
-              <div
-                key={index}
-                className="relative bg-white w-96 h-full  rounded-b-lg text-[#002147] shadow-lg hover:bg-yellow-400 transition-all duration-300 ease-in-out transform hover:scale-110 hover:shadow-lg">
-                <div className="absolute inset-x-0 -top-0 flex justify-center">
-                  <div className="bg-blue-100 rounded-full w-32 h-32 mt-2">
-                    {card.icon}
-                  </div>
-                </div>
-                <div className=" bg-[#002147] h-16 m-0 w-96 "></div>
-                <div className="pt-24 p-12 text-center ">
-                  <h2 className="text-2xl font-bold mb-4 text-[#53CCDC]">
-                    {card.title}
-                  </h2>
-                  <p className="text-6xl font-semibold">
-                    {card.price}
-                  </p>
-                  <p className="text-gray-700 mb-4 mt-4">
-                    {card.description}
-                  </p>
-                  <ul className="list-disc list-inside mb-4 text-left ml-16">
-                    {card.features.map((feature, index) => (
-                      <li key={index}>{feature}</li>
-                      ))}
-                      </ul>
-                  <button
-                    className="bg-[#002147] hover:bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-48"
-                    type="button">
-                    Submit Request
-                  </button>
-                </div>
-              </div>
-            ))}
-        </div>
-      </section> */}
 
-<section>
-  <div></div>
-      <div className="bg-[#002147] p-8 flex justify-around items-start">
-        {cardData.map((card, index) => (
-          <div
-            key={index}
-            className="relative bg-white w-96 h-full rounded-b-lg  text-[#002147] shadow-lg group hover:bg-blue-100 transition-all duration-300 ease-in-out transform hover:scale-110 hover:shadow-lg">
-            <div className="absolute inset-x-0 -top-0 flex justify-center">
-              <div className="bg-blue-100 rounded-full w-32 h-32 mt-2 transition-all duration-300 ease-in-out group-hover:bg-white">
-                {card.icon}
+        <div className="p-16 pt-24 flex space-x-8 text-[#002147]">
+      
+          <div className="content flex-grow pt-16 text-left">
+            <h2 className="text-l font-bold mb-4 text-left text-[#53CCDC]">
+              Why Choose Us
+            </h2>
+            <p className="text-4xl mb-4 text-left font-bold ">
+              We Will Make Any Place Absolutely Clean, Neat.
+            </p>
+            <p className="mb-8 text-left">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam.
+            </p>
+
+            <ul className="list-none mb-4 grid grid-cols-2 gap-x-4 gap-y-2 font-bold">
+              <li className="flex items-center mb-2">
+                <svg
+                  className="w-5 h-5 text-green-500 mr-2"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                Experienced Team
+              </li>
+              <li className="flex items-center mb-2">
+                <svg
+                  className="w-5 h-5 text-green-500 mr-2"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                Weekly or fortnightly
+              </li>
+              <li className="flex items-center mb-2">
+                <svg
+                  className="w-5 h-5 text-green-500 mr-2"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                All in all service
+              </li>
+              <li className="flex items-center mb-2">
+                <svg
+                  className="w-5 h-5 text-green-500 mr-2"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                Book, manage & pay
+              </li>
+            </ul>
+
+     
+
+
+          </div>
+          <div className="img flex-shrink-0 w-2/5 pt-8 ">
+            <img
+              src="/src/assets/12.jpeg"
+              alt="Demo pic"
+              className="rounded-lg shadow-2xl w-48 h-48 object-cover relative top-3"
+            />
+             <img
+              src="/src/assets/pic 3.jpeg"
+              alt="Demo pic"
+              className="rounded-xl shadow-2xl w-48 h-48 object-cover relative left-32 z-10"
+            />
+          </div>
+
+        </div>
+      </section>
+
+      <section>
+        <div></div>
+        <div className="bg-[#002147] p-8 flex justify-around items-start">
+          {cardData.map((card, index) => (
+            <div
+              key={index}
+              className="relative bg-white w-96 h-full rounded-b-lg  text-[#002147] shadow-lg group hover:bg-blue-100 transition-all duration-300 ease-in-out transform hover:scale-110 hover:shadow-lg">
+              <div className="absolute inset-x-0 -top-0 flex justify-center">
+                <div className="bg-blue-100 rounded-full w-32 h-32 mt-2 transition-all duration-300 ease-in-out group-hover:bg-white">
+                  {card.icon}
+                </div>
+              </div>
+              <div className="bg-[#002147] h-16 m-0 w-full border border-transparent"></div>
+              <div className="pt-24 p-12 text-center">
+                <h2 className="text-2xl font-bold mb-4 text-[#53CCDC]">
+                  {card.title}
+                </h2>
+                <p className="text-6xl font-semibold">{card.price}</p>
+                <p className="text-gray-700 mb-4 mt-4">{card.description}</p>
+                <ul className="list-disc list-inside mb-4 text-left ml-16">
+                  {card.features.map((feature, index) => (
+                    <li key={index}>{feature}</li>
+                  ))}
+                </ul>
+                <button
+                  className="bg-[#002147] hover:bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-48"
+                  type="button">
+                  Submit Request
+                </button>
               </div>
             </div>
-            <div className="bg-[#002147] h-16 m-0 w-full border border-transparent"></div>
-            <div className="pt-24 p-12 text-center">
-              <h2 className="text-2xl font-bold mb-4 text-[#53CCDC]">
-                {card.title}
-              </h2>
-              <p className="text-6xl font-semibold">
-                {card.price}
-              </p>
-              <p className="text-gray-700 mb-4 mt-4">
-                {card.description}
-              </p>
-              <ul className="list-disc list-inside mb-4 text-left ml-16">
-                {card.features.map((feature, index) => (
-                  <li key={index}>{feature}</li>
-                ))}
-              </ul>
-              <button
-                className="bg-[#002147] hover:bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-48"
-                type="button">
-                Submit Request
-              </button>
-            </div>
-          </div>
-        ))}
-      </div>
-    </section>
+          ))}
+        </div>
+      </section>
 
       <section
         className="bg-cover bg-center relative w-screen mx-auto pt-15 "
